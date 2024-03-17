168534c4b3366d68c9522bc0384c02fcdd9313cc
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-18 00:53:28 +0100

    created login form with zod validation

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,43 +1,98 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+const loginSchema = z
+  .object({
+    username: z.string(),
+    password: z.string(),
+  })
+  .refine(
+    (data) => {
+      if (!data.username) return;
+      return true;
+    },
+    {
+      message: "Username required!",
+      path: ["username"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (!data.password) return;
+      return true;
+    },
+    {
+      message: "Password required!",
+      path: ["password"],
+    }
+  );
 
 const Login = () => {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const handleLogin = (values: z.infer<typeof loginSchema>) => {
+    console.log(values);
+  };
+
   return (
     <>
-      <div className="h-[50px] w-full mb-20 bg-black"></div>
-      <Card className="mx-auto max-w-sm border-none">
-        <CardHeader className="space-y-1 text-center uppercase">
-          <CardTitle className="text-2xl font-bold">Login</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="mb-8">
-            <Input
-              id="username"
-              placeholder="Enter username"
-              required
-              type="text"
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleLogin)}
+          className="max-w-sm mx-auto mt-20 px-4 sm:px-0"
+        >
+          <h3 className="py-2 text-center font-bold text-2xl">LOGIN</h3>
+          <div className="space-y-4">
+            <FormField
               name="username"
-              className="rounded-full shadow-lg placeholder:text-slate-300 text-lg"
+              control={form.control}
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormControl>
+                      <Input {...field} placeholder="Enter username" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormControl>
+                      <Input {...field} placeholder="Enter password" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
             />
           </div>
-          <div className="space-y-2">
-            <Input
-              id="password"
-              required
-              type="password"
-              placeholder="Enter password"
-              className="rounded-full shadow-lg placeholder:text-slate-300 text-lg"
-            />
-          </div>
-          <Button
-            className="w-full bg-black text-lg rounded-full shadow-lg mt-20"
-            type="submit"
-          >
-            Login
-          </Button>
-        </CardContent>
-      </Card>
+          <Button className="w-full mt-20">Login</Button>
+        </form>
+      </Form>
     </>
   );
 };
