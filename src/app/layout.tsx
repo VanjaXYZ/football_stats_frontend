8b22b2cd7a01852dfa586b05716f7a61fdfcf643
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-18 22:41:55 +0100

    add image && topnavmenu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
+import TopNavMenu from "./(navigation)/TopNavMenu";
+import FS_WHITE_LOGO from "@/assets/logo bijela.png";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"] });
@@ -17,7 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <body className={montserrat.className}>
+        <div className="bg-black/90 h-16 px-6 flex items-center py-10">
+          <Image src={FS_WHITE_LOGO} alt="fs_logo" width={50} height={50} />
+          <TopNavMenu />
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
