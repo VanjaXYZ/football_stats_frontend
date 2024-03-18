b170511d9ccb8457e96f857aab2c7235e64ce01f
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-18 23:23:10 +0100

    created

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import HeroSection from "../(shared)/HeroSection";
 
 const Homepage = () => {
-  return <div>Hello from Homepage...</div>;
+  return (
+    <div className=" w-full min-h-[100vh]">
+      <HeroSection />
+      <div>Hello from Homepage...</div>
+    </div>
+  );
 };
 
 export default Homepage;
