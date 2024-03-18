e1551259da7b671cc4d305d89cbe01a46ea4e42c
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-18 23:22:59 +0100

    reconsturciton

--- src/app/(navigation)/TopNavMenu.tsx
+++ src/app/(navigation)/TopNavMenu.tsx
@@ -1,60 +1,44 @@
 "use client";
-import React from "react";
-import { usePathname } from "next/navigation";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const TopNavMenu = () => {
   const pathname = usePathname();
 
   return (
     pathname !== "/login" && (
-      <NavigationMenu className="ms-auto flex gap-16 items-center">
-        <NavigationMenuItem className="list-none">
-          <Link href={"/home"} className="text-white text-xl">
-            <NavigationMenuLink
-              className={
-                pathname === "/home" ? "underline underline-offset-4" : ""
-              }
-            >
-              Home
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem className="list-none">
-          <Link href={"/other-stats"} className="text-white text-xl">
-            <NavigationMenuLink
-              className={
-                pathname === "/other-stats"
-                  ? "underline underline-offset-4"
-                  : ""
-              }
-            >
-              Other stats
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem className="list-none">
-          <Link href={"/about"} className="text-white text-xl">
-            <NavigationMenuLink
-              className={
-                pathname === "/about" ? "underline underline-offset-4" : ""
-              }
-            >
-              About
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenu>
+      <div className="ms-auto flex gap-16 items-center">
+        <Link
+          href={"/home"}
+          className={
+            pathname === "/home"
+              ? "underline underline-offset-4 text-white text-xl"
+              : "text-white text-xl"
+          }
+        >
+          Home
+        </Link>
+        <Link
+          href={"/other-stats"}
+          className={
+            pathname === "/other-stats"
+              ? "underline underline-offset-4 text-white text-xl"
+              : "text-white text-xl"
+          }
+        >
+          Other stats
+        </Link>
+        <Link
+          href={"/about"}
+          className={
+            pathname === "/about"
+              ? "underline underline-offset-4 text-white text-xl"
+              : "text-white text-xl"
+          }
+        >
+          About
+        </Link>
+      </div>
     )
   );
 };
