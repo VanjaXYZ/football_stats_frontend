2bf418df014fb3272823172b2199b4afe4ea853a
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-18 22:42:20 +0100

    resturctured

--- src/app/(navigation)/TopNavMenu.tsx
+++ src/app/(navigation)/TopNavMenu.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import { usePathname } from "next/navigation";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -10,19 +11,51 @@ import {
   NavigationMenuTrigger,
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
+import Link from "next/link";
 
 const TopNavMenu = () => {
+  const pathname = usePathname();
+
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Home</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
+    pathname !== "/login" && (
+      <NavigationMenu className="ms-auto flex gap-16 items-center">
+        <NavigationMenuItem className="list-none">
+          <Link href={"/home"} className="text-white text-xl">
+            <NavigationMenuLink
+              className={
+                pathname === "/home" ? "underline underline-offset-4" : ""
+              }
+            >
+              Home
+            </NavigationMenuLink>
+          </Link>
         </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+        <NavigationMenuItem className="list-none">
+          <Link href={"/other-stats"} className="text-white text-xl">
+            <NavigationMenuLink
+              className={
+                pathname === "/other-stats"
+                  ? "underline underline-offset-4"
+                  : ""
+              }
+            >
+              Other stats
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="list-none">
+          <Link href={"/about"} className="text-white text-xl">
+            <NavigationMenuLink
+              className={
+                pathname === "/about" ? "underline underline-offset-4" : ""
+              }
+            >
+              About
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenu>
+    )
   );
 };
 
