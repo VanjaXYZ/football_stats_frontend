2105cd83b677dca79b69528cfbb707948f98c5c0
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-23 12:11:20 +0100

    add last_10_games

--- src/app/(pages)/home/components/StatsBetweenTeams.tsx
+++ src/app/(pages)/home/components/StatsBetweenTeams.tsx
@@ -2,6 +2,7 @@
 import { ChevronDown } from "lucide-react";
 import SelectComponent from "../../(shared)/SelectComponent";
 import { useStatsBetweenStore } from "@/store/StatsBetweenStore";
+import TableComponent from "../../(shared)/TableComponent";
 
 const StatsBetweenTeams = () => {
   const countriesData = [
@@ -41,6 +42,180 @@ const StatsBetweenTeams = () => {
     "West Ham",
   ];
 
+  const homeTeamStats = [
+    {
+      gameNumber: 287,
+      homeWins: 219,
+      awayWins: 47,
+      draws: 21,
+      _0_1: 100,
+      _0_2: 120,
+      _3: 120,
+      _4: 44,
+      _5: 3,
+      _6: 2,
+      _7: 0,
+      _gg: 0,
+      avg_goals: 2.78,
+      avg_halftime_goals: 1.12,
+    },
+  ];
+
+  const homeTeamStatsHeaders = [
+    {
+      key: "gameNumber",
+      header: "Game number",
+    },
+    {
+      key: "homeWins",
+      header: "Home wins",
+    },
+    {
+      key: "awayWins",
+      header: "Away wins",
+    },
+    {
+      key: "draws",
+      header: "Draws",
+    },
+    {
+      key: "_0_1",
+      header: "0-1",
+    },
+    {
+      key: "_0_2",
+      header: "0-2",
+    },
+    {
+      key: "_3",
+      header: "3+",
+    },
+    {
+      key: "_4",
+      header: "4+",
+    },
+    {
+      key: "_5",
+      header: "5+",
+    },
+    {
+      key: "_6",
+      header: "6+",
+    },
+    {
+      key: "_7",
+      header: "7+",
+    },
+    {
+      key: "_gg",
+      header: "GG",
+    },
+    {
+      key: "avg_goals",
+      header: "AVG goals",
+    },
+    {
+      key: "avg_halftime_goals",
+      header: "AVG halftime goals",
+    },
+  ];
+
+  const last_10_games = [
+    {
+      key: "home",
+      header: "Home",
+    },
+    {
+      key: "away",
+      header: "Away",
+    },
+    {
+      key: "result",
+      header: "Result",
+    },
+    {
+      key: "halftime_result",
+      header: "Halftime result",
+    },
+    {
+      key: "date",
+      header: "Date",
+    },
+  ];
+
+  const last_10_games_data = [
+    {
+      home: "Liverpool",
+      away: "Aresnal",
+      result: "3-2",
+      halftime_result: "0-0",
+      date: "13.09.2022",
+    },
+    {
+      home: "Liverpool",
+      away: "West Ham",
+      result: "1-1",
+      halftime_result: "1-0",
+      date: "19.11.2022",
+    },
+    {
+      home: "Liverpool",
+      away: "Man City",
+      result: "0-0",
+      halftime_result: "0-0",
+      date: "03.01.2024",
+    },
+    {
+      home: "Liverpool",
+      away: "Tottenham",
+      result: "1-3",
+      halftime_result: "0-2",
+      date: "18.02.2024",
+    },
+    {
+      home: "Liverpool",
+      away: "Fulham",
+      result: "2-0",
+      halftime_result: "2-0",
+      date: "20.02.2024",
+    },
+    {
+      home: "Liverpool",
+      away: "Man Utd",
+      result: "4-4",
+      halftime_result: "3-2",
+      date: "27.02.2024",
+    },
+    {
+      home: "Liverpool",
+      away: "Everton",
+      result: "2-1",
+      halftime_result: "1-1",
+      date: "28.02.2024",
+    },
+    {
+      home: "Liverpool",
+      away: "Aston Villa",
+      result: "5-0",
+      halftime_result: "2-0",
+      date: "04.03.2024",
+    },
+    {
+      home: "Liverpool",
+      away: "Brighton",
+      result: "0-1",
+      halftime_result: "0-0",
+      date: "10.03.2024",
+    },
+    {
+      home: "Liverpool",
+      away: "Burnley",
+      result: "3-0",
+      halftime_result: "3-0",
+      date: "15.03.2024",
+    },
+  ];
+
   const { setCountry, setHomeTeam, setAwayTeam, stats } =
     useStatsBetweenStore() as any;
 
@@ -56,7 +231,6 @@ const StatsBetweenTeams = () => {
     setAwayTeam(e);
     console.log("Away team: ", e);
   };
-  console.log(stats);
   return (
     <>
       <section>
@@ -101,6 +275,17 @@ const StatsBetweenTeams = () => {
           )}
         </div>
       </section>
+      {stats.homeTeam && (
+        <>
+          <section>
+            <TableComponent column={homeTeamStatsHeaders} row={homeTeamStats} />
+            <h3 className="bg-black text-white px-14 py-4 font-semibold">
+              Last 10 games
+            </h3>
+            <TableComponent column={last_10_games} row={last_10_games_data} />
+          </section>
+        </>
+      )}
     </>
   );
 };
