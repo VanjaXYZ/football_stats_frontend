c9becfc53d5744c665418ac67515e98ae2ce821a
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-23 13:40:36 +0100

    created

---
+++ src/app/(pages)/dashboard/@zero_one/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ZeroToOne = () => {
+  return (
+    <div className="flex justify-between items-center min-w-[280px] text-white bg-black p-4 rounded-full">
+      <span className="text-xl font-bold ms-8">0-1</span>
+      <span className="py-1 px-4 border bg-green-400 rounded-full">Done</span>
+    </div>
+  );
+};
+
+export default ZeroToOne;
---
+++ src/app/(pages)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="pb-10 text-2xl uppercase font-semibold">
+      Football Stats Dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
