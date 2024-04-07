c62110dcd4fafb20eb06b3096a785ec2b498ec1e
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-07 21:37:00 +0100

    add countires and selcet component

--- src/app/(pages)/other-stats/components/NumberOfGoals.tsx
+++ src/app/(pages)/other-stats/components/NumberOfGoals.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@/components/ui/button";
 import React from "react";
+import SelectComponent from "../../(shared)/SelectComponent";
 
 const NumberOfGoals = () => {
-  const numberOfGoalsData = [
+  const numberOfGoalsData: string[] | number = [
     "No goals",
     "0-1",
     "0-2",
@@ -13,12 +14,29 @@ const NumberOfGoals = () => {
     "7+",
     "1-2/2-1",
   ];
+  const countries: string[] = [
+    "Japan",
+    "Brazil",
+    "France",
+    "Kenya",
+    "Canada",
+    "Russia",
+    "Italy",
+    "Mexico",
+    "Nigeria",
+    "Germany",
+  ];
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 px-4 py-8">
-      {numberOfGoalsData.map((goals: string, index: number) => (
-        <Button key={index}>{goals}</Button>
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 px-4 py-8">
+        {numberOfGoalsData.map((goals, index) => (
+          <Button key={index}>{goals}</Button>
+        ))}
+      </div>
+      <div className="flex justify-center ">
+        <SelectComponent />
+      </div>
+    </>
   );
 };
 
