55bd7748250bcbbed856abd8f4a91918b55cbce9
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-07 22:00:01 +0100

    css fix

--- src/app/(pages)/(shared)/Footer.tsx
+++ src/app/(pages)/(shared)/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="border-t text-center">
+    <footer className="border-t text-center bg-gray-500/40 font-semibold">
       &copy; {new Date().getFullYear()}. Designed by <b>Tea Jaćimović</b> for{" "}
       <b>Boris Bašić</b>. All rights reserved.
     </footer>
