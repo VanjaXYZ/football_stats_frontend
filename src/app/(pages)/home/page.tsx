11d8be2ce43ac1df85a2f187a1da5ff2c6a9c499
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-10 23:46:29 +0100

    add elo rating & quote percent

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -9,22 +9,52 @@ const Homepage = () => {
     {
       key: "homeTeam",
       header: "home",
+      Component: (item: any) => (
+        <div className="flex flex-col items-center">
+          <span>{item.homeTeam}</span>
+          <span>(ELO: {item?.eloHomeTeam})</span>
+        </div>
+      ),
     },
     {
       key: "awayTeam",
       header: "away",
+      Component: (item: any) => (
+        <div className="flex flex-col items-center">
+          <span>{item.awayTeam}</span>
+          <span>(ELO: {item?.eloAwayTeam})</span>
+        </div>
+      ),
     },
     {
       key: "_1_quote",
       header: "1",
+      Component: (item: any) => (
+        <div className="flex flex-col items-center">
+          <span>{item._1_quote}</span>
+          <span>{item?._1_prediction}%</span>
+        </div>
+      ),
     },
     {
       key: "_x_quote",
       header: "x",
+      Component: (item: any) => (
+        <div className="flex flex-col items-center">
+          <span>{item._x_quote}</span>
+          <span>{item?._x_prediction}%</span>
+        </div>
+      ),
     },
     {
       key: "_2_quote",
       header: "2",
+      Component: (item: any) => (
+        <div className="flex flex-col items-center">
+          <span>{item._2_quote}</span>
+          <span>{item?._2_prediction}%</span>
+        </div>
+      ),
     },
     {
       key: "date",
@@ -34,15 +64,35 @@ const Homepage = () => {
   const homepageRowData = [
     {
       homeTeam: "Manchester City",
+      eloHomeTeam: 2019,
       awayTeam: "Arsenal",
+      eloAwayTeam: 1992,
       _1_quote: 1.55,
+      _1_prediction: 75,
       _x_quote: 1.26,
+      _x_prediction: 15,
       _2_quote: 1.76,
+      _2_prediction: 10,
       date: "03.07.2023.",
     },
     {
       homeTeam: "Crystal Palace",
+      eloHomeTeam: 1243,
       awayTeam: "Newcastle",
+      eloAwayTeam: 1890,
+      _1_quote: 3.12,
+      _1_prediction: 45,
+      _x_quote: 2.01,
+      _x_prediction: 30,
+      _2_quote: 1.3,
+      _2_prediction: 25,
+      date: "20.06.2023.",
+    },
+    {
+      homeTeam: "Crystal Palace",
+      eloHomeTeam: 1243,
+      awayTeam: "Newcastle",
+      eloAwayTeam: 1890,
       _1_quote: 3.12,
       _x_quote: 2.01,
       _2_quote: 1.3,
@@ -50,15 +100,9 @@ const Homepage = () => {
     },
     {
       homeTeam: "Crystal Palace",
+      eloHomeTeam: 1243,
       awayTeam: "Newcastle",
-      _1_quote: 3.12,
-      _x_quote: 2.01,
-      _2_quote: 1.3,
-      date: "20.06.2023.",
-    },
-    {
-      homeTeam: "Crystal Palace",
-      awayTeam: "Newcastle",
+      eloAwayTeam: 1890,
       _1_quote: 3.12,
       _x_quote: 2.01,
       _2_quote: 1.3,
