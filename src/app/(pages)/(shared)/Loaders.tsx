2f1f612925231cfe95251352c1a14c3eec31ecbc
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-28 17:11:09 +0100

    created TableLoading skeleton

--- src/app/(pages)/(shared)/Loaders.tsx
+++ src/app/(pages)/(shared)/Loaders.tsx
@@ -1,1 +1,15 @@
-export const TableLoading = () => {};
+import { Skeleton } from "@/components/ui/skeleton";
+
+export const TableLoading = () => {
+  return (
+    <div className="flex flex-col items-center space-y-4">
+      <Skeleton className="w-[320px] h-[50px] text-4xl mt-12" />
+      <div className="space-y-2 w-full">
+        <Skeleton className="w-full h-14" />
+        <Skeleton className="w-full h-14" />
+        <Skeleton className="w-full h-14" />
+        <Skeleton className="w-full h-14" />
+      </div>
+    </div>
+  );
+};
