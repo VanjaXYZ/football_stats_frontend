dfb603b2e3d9c542da6a0d0a0811913a4164577f
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-28 17:11:29 +0100

    created

---
+++ src/app/routes/Predicted_games/routes.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import axios from "axios";
+import { Config } from "../../../../Config";
+
+export const getPredictedGames = async () => {
+  try {
+    const response = await axios.get(`${Config.baseURL}/quotes`);
+    if (response?.status === 200) {
+      return response?.data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
