53c079fc60559de9fd1edc4c4718c293318f763e
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-03 22:59:41 +0100

    created

---
+++ src/app/(pages)/home/stats_between_teams/ShowHistoryBetweenTeamsButton.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useStatsBetweenStore } from "@/store/StatsBetweenStore";
+import { useRouter } from "next/navigation";
+import { getStatsBetweenTeams } from "../../../routes/Stats_Between_Teams/routes";
+
+const ShowHistoryBetweenTeamsButton = () => {
+  const { stats } = useStatsBetweenStore() as any;
+  const router = useRouter();
+
+  const onGetStatsBetweenTeams = async (
+    country: string,
+    team_1: string,
+    team_2: string
+  ) => {
+    const response = await getStatsBetweenTeams(country, team_1, team_2);
+    router.push(
+      `home/stats_between_teams/${country}/${team_1.replaceAll(
+        " ",
+        "_"
+      )}/${team_2.replaceAll(" ", "_")}`
+    );
+    return response;
+  };
+
+  return (
+    stats.homeTeam && (
+      <Button
+        onClick={() =>
+          onGetStatsBetweenTeams(
+            stats.country.country,
+            stats.homeTeam,
+            stats.awayTeam
+          )
+        }
+      >
+        Show
+      </Button>
+    )
+  );
+};
+
+export default ShowHistoryBetweenTeamsButton;
