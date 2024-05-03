c91f63deb0e17362cce25b02725db54a9eb21fc9
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-04 00:21:37 +0100

    created

--- src/store/StatsBetweenStore.ts
+++ src/store/StatsBetweenStore.ts
@@ -1,16 +1,27 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const useStatsBetweenStore = create((set) => ({
-  stats: {
-    country: "",
-    homeTeam: "",
-    awayTeam: "",
-  },
+let dataFromLocalStorage: any = global?.localStorage?.getItem("stats");
 
-  setCountry: (country: string) =>
-    set((state: any) => ({ stats: { ...state.stats, country: country } })),
-  setHomeTeam: (team: string) =>
-    set((state: any) => ({ stats: { ...state.stats, homeTeam: team } })),
-  setAwayTeam: (team: string) =>
-    set((state: any) => ({ stats: { ...state.stats, awayTeam: team } })),
-}));
+export const useStatsBetweenStore = create(
+  persist(
+    (set) => ({
+      stats: {
+        country: dataFromLocalStorage?.state?.stats?.country || "",
+        homeTeam: dataFromLocalStorage?.state?.stats?.homeTeam || "",
+        awayTeam: dataFromLocalStorage?.state?.stats?.awayTeam || "",
+      },
+
+      setCountry: (country: any) =>
+        set((state: any) => ({ stats: { ...state.stats, country: country } })),
+      setHomeTeam: (team: any) =>
+        set((state: any) => ({ stats: { ...state.stats, homeTeam: team } })),
+      setAwayTeam: (team: any) =>
+        set((state: any) => ({ stats: { ...state.stats, awayTeam: team } })),
+    }),
+    {
+      name: "stats",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
