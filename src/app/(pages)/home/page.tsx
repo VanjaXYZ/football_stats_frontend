aee5e22bcf8fc1a1ce6945ee2587ee93f27e3dda
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-03 22:59:16 +0100

    renamed component

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,14 +1,14 @@
 import { Suspense } from "react";
 import HeroSection from "../(shared)/HeroSection";
-import StatsBetweenTeams from "./components/stats_between_teams/StatsBetweenTeams";
-import { GamePredictions } from "./components/game_predictions/GamePredictions";
 import { TableLoading } from "../(shared)/Loaders";
+import { GamePredictions } from "./components/game_predictions/GamePredictions";
+import ChooseTeams from "./stats_between_teams/ChooseTeams";
 const Homepage = () => {
   return (
     <main className=" w-full min-h-[100vh]">
       <HeroSection />
       <section>
-        <StatsBetweenTeams />
+        <ChooseTeams />
       </section>
       <Suspense fallback={<TableLoading />}>
         <GamePredictions />
