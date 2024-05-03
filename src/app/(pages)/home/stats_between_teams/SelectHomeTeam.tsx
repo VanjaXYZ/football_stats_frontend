62e2e8994e6fca2c40194a7f16c7544381215b6c
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-03 22:59:36 +0100

    created

---
+++ src/app/(pages)/home/stats_between_teams/SelectHomeTeam.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { getTeams } from "@/app/routes/Stats_Between_Teams/routes";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useStatsBetweenStore } from "@/store/StatsBetweenStore";
+import { useEffect, useState } from "react";
+
+const SelectHomeTeam = () => {
+  const { setHomeTeam, stats } = useStatsBetweenStore() as any;
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    const fetchTeams = async () => {
+      const teamsData = await getTeams(stats?.country?.country);
+      setTeams(teamsData?.teams);
+    };
+
+    fetchTeams();
+  }, [stats.country]);
+
+  return (
+    <div>
+      {stats.country && (
+        <Select
+          onValueChange={(e: any) => setHomeTeam(e)}
+          value={stats.homeTeam}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder={`Select home team`} />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Home Team</SelectLabel>
+              {teams?.map((item: any) => (
+                <SelectItem value={item} key={item}>
+                  {item}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      )}
+    </div>
+  );
+};
+
+export default SelectHomeTeam;
