bc43e85eaa4c637352bb2f7d5f882c2c6dbfdb37
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-04 12:20:04 +0100

    created

---
+++ src/app/(pages)/home/stats_between_teams/TeamVsTeamStats.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import TableComponent from "../../(shared)/TableComponent";
+
+const TeamVsTeamStats = ({ stats }: { stats: any }) => {
+  console.log(stats);
+  const headerTableData = [
+    {
+      key: "",
+      header: "Home",
+      Component: (item: any) => <span>{item[1]}</span>,
+    },
+    {
+      key: "",
+      header: "Away",
+      Component: (item: any) => <span>{item[2]}</span>,
+    },
+    {
+      key: "",
+      header: "Result",
+      Component: (item: any) => <span>{item[3]}</span>,
+    },
+    {
+      key: "",
+      header: "Halftime Result",
+      Component: (item: any) => <span>{item[4]}</span>,
+    },
+    {
+      key: "",
+      header: "Date",
+      Component: (item: any) => <span>{item[0]}</span>,
+    },
+  ];
+  return (
+    <div className="py-4 space-y-2">
+      <h3 className="text-xl font-semibold">Last 10 games</h3>
+      <TableComponent column={headerTableData} row={stats} />
+    </div>
+  );
+};
+
+export default TeamVsTeamStats;
