1056d28f4a76dacb62198038b47d06fcf28c9b19
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-04 12:19:36 +0100

    created

--- src/app/(pages)/home/stats_between_teams/page.tsx
+++ src/app/(pages)/home/stats_between_teams/page.tsx
@@ -1,7 +1,151 @@
-import React from "react";
+"use client";
+import { getStatsBetweenTeams } from "@/app/routes/Stats_Between_Teams/routes";
+import { useStatsBetweenStore } from "@/store/StatsBetweenStore";
+import React, { useEffect, useState } from "react";
+import TableComponent from "../../(shared)/TableComponent";
+import TeamVsTeamHeader from "./TeamVsTeamHeader";
+import TeamVsTeamStats from "./TeamVsTeamStats";
 
 const StatsBetweenTeams = () => {
-  return <div>test</div>;
+  const { stats } = useStatsBetweenStore() as any;
+  const [teamsScore, setTeamsScore] = useState<any[]>([]);
+  useEffect(() => {
+    const fetchStats = async () => {
+      try {
+        const getStats = await getStatsBetweenTeams(
+          stats?.country,
+          stats?.homeTeam,
+          stats?.awayTeam
+        );
+        setTeamsScore([getStats]);
+      } catch (error) {
+        // Handle error if necessary
+        console.error("Error fetching stats:", error);
+      }
+    };
+
+    fetchStats();
+  }, [stats?.country, stats?.homeTeam, stats?.awayTeam]);
+  const topTableData = [
+    {
+      key: "game_number",
+      header: "Game number",
+      Component: (item: any) => <span>{item?.game_number}</span>,
+    },
+    {
+      key: "home_wins",
+      header: "Home wins",
+      Component: (item: any) => <span>{item?.home_wins}</span>,
+    },
+    {
+      key: "away_wins",
+      header: "Away wins",
+      Component: (item: any) => <span>{item?.away_wins}</span>,
+    },
+    {
+      key: "draws",
+      header: "Draws",
+      Component: (item: any) => <span>{item?.draws}</span>,
+    },
+    {
+      key: "nula_1",
+      header: "0-1",
+      Component: (item: any) => (
+        <div className="flex flex-col items-center">
+          <span>{item?.nula_1[0]}</span>
+          <span>{Number(item?.nula_1[1] * 100).toFixed()}%</span>
+        </div>
+      ),
+    },
+    {
+      key: "nula_2",
+      header: "0-2",
+      Component: (item: any) => (
+        <div className="flex flex-col items-center">
+          <span>{item?.nula_2[0]}</span>
+          <span>{Number(item?.nula_2[1] * 100).toFixed()}%</span>
+        </div>
+      ),
+    },
+    {
+      key: "plus_3",
+      header: "3+",
+      Component: (item: any) => (
+        <div className="flex flex-col items-center">
+          <span>{item?.plus_3[0]}</span>
+          <span>{Number(item?.plus_3[1] * 100).toFixed()}%</span>
+        </div>
+      ),
+    },
+    {
+      key: "plus_4",
+      header: "4+",
+      Component: (item: any) => (
+        <div className="flex flex-col items-center">
+          <span>{item?.plus_4[0]}</span>
+          <span>{Number(item?.plus_4[1] * 100).toFixed()}%</span>
+        </div>
+      ),
+    },
+    {
+      key: "plus_5",
+      header: "5+",
+      Component: (item: any) => (
+        <div className="flex flex-col items-center">
+          <span>{item?.plus_5[0]}</span>
+          <span>{Number(item?.plus_5[1] * 100).toFixed()}%</span>
+        </div>
+      ),
+    },
+    {
+      key: "plus_6",
+      header: "6+",
+      Component: (item: any) => (
+        <div className="flex flex-col items-center">
+          <span>{item?.plus_6[0]}</span>
+          <span>{Number(item?.plus_6[1] * 100).toFixed()}%</span>
+        </div>
+      ),
+    },
+    {
+      key: "plus_7",
+      header: "7+",
+      Component: (item: any) => (
+        <div className="flex flex-col items-center">
+          <span>{item?.plus_7[0]}</span>
+          <span>{Number(item?.plus_7[1] * 100).toFixed()}%</span>
+        </div>
+      ),
+    },
+    {
+      key: "goal_goal",
+      header: "gg",
+      Component: (item: any) => (
+        <div className="flex flex-col items-center">
+          <span>{item?.goal_goal[0]}</span>
+          <span>{Number(item?.goal_goal[1] * 100).toFixed()}%</span>
+        </div>
+      ),
+    },
+    {
+      key: "avg_goals",
+      header: "avg goals",
+      Component: (item: any) => <span>{item?.avg_goals}</span>,
+    },
+    {
+      key: "avg_half_time_goals",
+      header: "avg halftime goals",
+      Component: (item: any) => <span>{item?.avg_half_time_goals}</span>,
+    },
+  ];
+
+  return (
+    <div className="mt-20">
+      <TeamVsTeamHeader homeTeam={stats.homeTeam} awayTeam={stats.awayTeam} />
+      <TableComponent column={topTableData} row={teamsScore} />
+      <TeamVsTeamStats stats={teamsScore[0]?.last_games} />
+    </div>
+  );
 };
 
 export default StatsBetweenTeams;
