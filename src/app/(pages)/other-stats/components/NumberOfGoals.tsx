bf221f2eb7ef802bcad39d47de66537975326992
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-06 02:26:18 +0100

    add urls

--- src/app/(pages)/other-stats/components/NumberOfGoals.tsx
+++ src/app/(pages)/other-stats/components/NumberOfGoals.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { getOverallStats } from "@/app/routes/Other_Stats/routes";
 import { Button } from "@/components/ui/button";
-import React from "react";
-import SelectComponent from "../../(shared)/SelectComponent";
 import { useOtherStats } from "@/store/OtherStatsStore";
+import { useState } from "react";
+import ShowNumberOfGoalsStats from "./ShowNumberOfGoalsStats";
 
 const NumberOfGoals = () => {
+  const { otherStats, setOtherStatsCountry } = useOtherStats() as any;
+  const [statsData, setStatsData] = useState([]);
   const numberOfGoalsData: string[] | number = [
     "No goals",
     "0-1",
@@ -16,27 +19,34 @@ const NumberOfGoals = () => {
     "7+",
     "1-2/2-1",
   ];
-  const countries: string[] = [
-    "Japan",
-    "Brazil",
-    "France",
-    "Kenya",
-    "Canada",
-    "Russia",
-    "Italy",
-    "Mexico",
-    "Nigeria",
-    "Germany",
+
+  const urls = [
+    "stats/no_goals",
+    "stats/one_goal_or_less",
+    "stats/two_goal_or_less",
+    "stats/plus_3",
+    "stats/plus_4",
+    "stats/plus_5",
+    "stats/plus_6",
+    "stats/plus_7",
+    "stats/two_one",
   ];
-  const { otherStats, setOtherStatsCountry } = useOtherStats() as any;
+  const onGetData = async (path: string[], position: number) => {
+    const data = await getOverallStats(path[position]);
+    setStatsData(data);
+    return data;
+  };
 
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 px-4 py-8">
         {numberOfGoalsData.map((goals, index) => (
-          <Button key={index}>{goals}</Button>
+          <Button key={index} onClick={() => onGetData(urls, index)}>
+            {goals}
+          </Button>
         ))}
       </div>
+      {statsData.length > 0 && <ShowNumberOfGoalsStats data={statsData} />}
     </>
   );
 };
