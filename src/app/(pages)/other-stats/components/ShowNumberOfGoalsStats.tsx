9d1c9e8bce14b2b85a3c6d07e18abcc55de42257
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-07 22:47:25 +0100

    add country to data table

--- src/app/(pages)/other-stats/components/ShowNumberOfGoalsStats.tsx
+++ src/app/(pages)/other-stats/components/ShowNumberOfGoalsStats.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import TableComponent from "../../(shared)/TableComponent";
 
+interface Item {
+  team: string;
+  country: string;
+  numberOfGames: number;
+}
+
 const ShowNumberOfGoalsStats = ({ data }: any) => {
   const tableHeaders = [
     {
       key: "team",
       header: "Team",
       Component: (item: any) => <div>{item?.team}</div>,
+    },
+    {
+      key: "country",
+      header: "Country",
+      Component: (item: Item) => {
+        for (let value of Object.values(item)) {
+          const single_value = value as Item;
+          if (single_value?.country) {
+            return <div>{single_value.country}</div>;
+          }
+        }
+      },
     },
     {
       key: "numberOfGames",
