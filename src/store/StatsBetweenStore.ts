53fd96a5f190ffc728e5938d3ed6073a13fa8055
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-07 22:11:19 +0100

    changed to sessionStorage

--- src/store/StatsBetweenStore.ts
+++ src/store/StatsBetweenStore.ts
@@ -21,7 +21,7 @@ export const useStatsBetweenStore = create(
     }),
     {
       name: "stats",
-      storage: createJSONStorage(() => localStorage),
+      storage: createJSONStorage(() => sessionStorage),
     }
   )
 );
