0f493c21c82217ffd3c4b0aa32ee457e7bd3cf05
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-08 15:45:22 +0100

    check if special stats exists

--- src/app/(pages)/other-stats/components/ShowNumberOfGoalsStats.tsx
+++ src/app/(pages)/other-stats/components/ShowNumberOfGoalsStats.tsx
@@ -8,11 +8,36 @@ interface Item {
 }
 
 const ShowNumberOfGoalsStats = ({ data }: any) => {
+  const checkIf_2_To_1 = data.some((element: any) => element.country_2_to_1);
+  const checkIf_Plus_7_All = data.some(
+    (element: any) => element.country_plus_7_all
+  );
   const tableHeaders = [
-    {
-      key: "team",
-      header: "Team",
-      Component: (item: any) => <div>{item?.team}</div>,
+    !checkIf_Plus_7_All && {
+      key: checkIf_2_To_1 ? "country_2_to_1" : "team",
+      header: checkIf_2_To_1 ? "Home team" : "Team",
+      Component: (item: any) => (
+        <div>{checkIf_2_To_1 ? item?.team_1 : item?.team}</div>
+      ),
+    },
+    checkIf_2_To_1 && {
+      key: "country_plus_7_all",
+      header: "Away team",
+      Component: (item: any) => <div>{item?.team_2}</div>,
+    },
+
+    checkIf_Plus_7_All && {
+      key: checkIf_Plus_7_All ? "country_plus_7_all" : "team",
+      header: checkIf_Plus_7_All ? "Home team" : "Team",
+      Component: (item: any) => (
+        <div>{checkIf_Plus_7_All ? item?.team_1 : item?.team}</div>
+      ),
+    },
+
+    checkIf_Plus_7_All && {
+      key: "country_plus_7_all",
+      header: "Away team",
+      Component: (item: any) => <div>{item?.team_2}</div>,
     },
     {
       key: "country",
