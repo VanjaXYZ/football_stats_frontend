4c4994ba8506ffb2c5c217431616c579d0fe7369
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-18 01:14:26 +0100

    created

--- src/store/OtherStatsStore.tsx
+++ src/store/OtherStatsStore.tsx
@@ -1,12 +1,28 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const useOtherStats = create((set) => ({
-  otherStats: {
-    country: "",
-  },
+let dataFromLocalStorage: any = global?.localStorage?.getItem("stats");
 
-  setOtherStatsCountry: (country: string) =>
-    set((state: any) => ({
-      otherStats: { ...state.otherStats, country: country },
-    })),
-}));
+export const useOtherStats = create(
+  persist(
+    (set) => ({
+      otherStats: {
+        country: dataFromLocalStorage?.state?.otherStats?.country || "",
+        activeUrl: dataFromLocalStorage?.state?.otherStats?.activeUrl || "",
+      },
+
+      setOtherStatsCountry: (country: string) =>
+        set((state: any) => ({
+          otherStats: { ...state.otherStats, country: country },
+        })),
+      setActiveUrl: (url: string) =>
+        set((state: any) => ({
+          otherStats: { ...state.otherStats, activeUrl: url },
+        })),
+    }),
+    {
+      name: "other-stats",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
