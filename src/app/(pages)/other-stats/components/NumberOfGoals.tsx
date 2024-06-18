878163d2ca37ba5d6964879b2cc294ab80fa16a7
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-18 14:27:33 +0100

    fix grid

--- src/app/(pages)/other-stats/components/NumberOfGoals.tsx
+++ src/app/(pages)/other-stats/components/NumberOfGoals.tsx
@@ -63,20 +63,20 @@ const NumberOfGoals = ({ countries }: { countries: any }) => {
 
   return (
     <>
-      <h2 className="px-4 py-2 text-4xl my-4 uppercase font-semibold text-center">
+      <h2 className="px-4 py-2 text-4xl my-4 uppercase font-semibold text-center antialiased">
         Stats
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 px-4 ">
+      <div className="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-5 px-4">
         {numberOfGoalsData.map((goals, index) => (
           <Button key={index} onClick={() => onGetData(urls, index)}>
             {goals}
           </Button>
         ))}
       </div>
-      <h2 className="px-4 py-2 text-4xl my-4 uppercase font-semibold text-center">
+      <h2 className="px-4 py-2 text-4xl my-4 uppercase font-semibold text-center antialiased">
         Special Stats
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 px-4 my-12">
+      <div className="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-5 px-4">
         {special_number_of_goals_data.map((goals, index) => (
           <Button key={index} onClick={() => onGetData(special_urls, index)}>
             {goals}
