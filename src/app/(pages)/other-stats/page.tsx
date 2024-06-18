feb0159ccbf374aba7334593a06e70ce50f39b45
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-18 23:36:56 +0100

    add metadata

--- src/app/(pages)/other-stats/page.tsx
+++ src/app/(pages)/other-stats/page.tsx
@@ -1,6 +1,11 @@
 import { getCountries } from "@/app/routes/Stats_Between_Teams/routes";
 import HeroSection from "../(shared)/HeroSection";
 import NumberOfGoals from "./components/NumberOfGoals";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Other stats",
+};
 
 const OtherStats = async () => {
   let countries = await getCountries();
