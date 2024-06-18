72f2836c5d1d922a2d0a222c7c5d1bae6d2dea55
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-18 01:13:46 +0100

    pass countries as props

--- src/app/(pages)/other-stats/page.tsx
+++ src/app/(pages)/other-stats/page.tsx
@@ -1,11 +1,13 @@
+import { getCountries } from "@/app/routes/Stats_Between_Teams/routes";
 import HeroSection from "../(shared)/HeroSection";
 import NumberOfGoals from "./components/NumberOfGoals";
 
 const OtherStats = async () => {
+  let countries = await getCountries();
   return (
     <>
       <HeroSection />
-      <NumberOfGoals />
+      <NumberOfGoals countries={countries} />
     </>
   );
 };
