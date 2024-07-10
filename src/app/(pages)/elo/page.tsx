2602ba2e79da0ca6c88304220702560eb1d6de9a
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-11 00:44:32 +0100

    careated

--- src/app/(pages)/elo/page.tsx
+++ src/app/(pages)/elo/page.tsx
@@ -1,15 +1,10 @@
-import React from "react";
 import HeroSection from "../(shared)/HeroSection";
-import { getBestELOTeams } from "@/app/routes/ELO_Stats/routes";
-import TopELOTeams from "./components/TopELOTeams";
 
 const Page = async () => {
-  const topEloTeams = await getBestELOTeams();
   return (
-    <>
+    <div className="w-full">
       <HeroSection />
-      <TopELOTeams topEloTeams={topEloTeams} />
-    </>
+    </div>
   );
 };
 
