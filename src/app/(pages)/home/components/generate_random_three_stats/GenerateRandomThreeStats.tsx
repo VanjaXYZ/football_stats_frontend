71faeb39d21b445f9f7dd5660db4c62a18e18551
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-10 15:13:31 +0100

    add heading

--- src/app/(pages)/home/components/generate_random_three_stats/GenerateRandomThreeStats.tsx
+++ src/app/(pages)/home/components/generate_random_three_stats/GenerateRandomThreeStats.tsx
@@ -6,6 +6,9 @@ const GenerateRandomThreeStats = async () => {
 
   return (
     <div>
+      <h2 className="text-center font-bold text-4xl uppercase py-8">
+        Random stats
+      </h2>
       <RandomThreeStatsTables data={randomThreeStats} />
     </div>
   );
