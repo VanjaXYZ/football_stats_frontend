9c4ad403d2d98151ea619f6bb907ade16aa72aee
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-10 14:47:01 +0100

    fix

--- src/app/(pages)/home/components/generate_random_three_stats/RandomThreeStatsTables.tsx.tsx
+++ src/app/(pages)/home/components/generate_random_three_stats/RandomThreeStatsTables.tsx.tsx
@@ -1,6 +1,8 @@
 "use client";
 import TableComponent from "@/app/(pages)/(shared)/TableComponent";
+import Image from "next/image";
 import React from "react";
+import { Config } from "../../../../../../Config";
 
 const RandomThreeStatsTables = ({ data }: any) => {
   let table_1 = data?.random_three[0];
@@ -17,28 +19,53 @@ const RandomThreeStatsTables = ({ data }: any) => {
       Component: (item: any) => <div>{item.stats.team}</div>,
     },
     {
+      key: "country",
+      header: "Country",
+      Component: (item: any) => {
+        for (let key in item?.stats) {
+          if (item?.stats[key]?.country) {
+            return (
+              <div className="flex items-center justify-between gap-x-6 max-w-32 m-auto">
+                {" "}
+                <Image
+                  src={`${Config.baseURL}/${item?.stats[
+                    key
+                  ]?.images_country[0]?.img_path?.replace("/", "")}`}
+                  alt={item?.stats[key]?.country}
+                  width={20}
+                  height={20}
+                />
+                {item?.stats[key]?.country}
+              </div>
+            );
+          }
+        }
+        return null; // Return null if no country is found
+      },
+    },
+    {
       key: "numberOfGames",
       header: "No. games",
       Component: (item: any) => <div>{item.stats.numberOfGames}</div>,
     },
   ];
   return (
-    <div className="flex justify-evenly">
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 px-2 md:gap-6 whitespace-nowrap">
       {hasTableLength && (
         <>
-          <div className="w-1/4">
-            <h2 className="text-center text-2xl bg-black text-white font-semibold">
+          <div className="w-full">
+            <h2 className="text-center text-2xl bg-black text-white font-semibold w-full">
               {table_1[0]?.type}
             </h2>
             <TableComponent column={columndData} row={table_1} />
           </div>
-          <div className="w-1/4">
+          <div className="">
             <h2 className="text-center text-2xl bg-black text-white font-semibold">
               {table_2[0]?.type}
             </h2>
             <TableComponent column={columndData} row={table_2} />
           </div>
-          <div className="w-1/4">
+          <div className="">
             <h2 className="text-center text-2xl bg-black text-white font-semibold">
               {table_3[0]?.type}
             </h2>
