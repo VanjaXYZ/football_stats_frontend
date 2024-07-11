41fde92fa0824a44ecc6836400ab4a4e085855d0
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-11 01:11:51 +0100

    add queris

--- src/app/(pages)/elo/@asideMenu/components/SearchBar.tsx
+++ src/app/(pages)/elo/@asideMenu/components/SearchBar.tsx
@@ -1,13 +1,34 @@
 "use client";
 import { Input } from "@/components/ui/input";
 import { SearchIcon } from "lucide-react";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
 import React from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 const SearchBar = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams);
+    console.log("Searching... ", term);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 500);
   return (
     <div className="relative">
       <SearchIcon className="absolute top-2 text-slate-400 left-2" />
-      <Input placeholder="Search country..." className="ps-10" />
+      <Input
+        placeholder="Search country..."
+        className="ps-10"
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get("query")?.toString()}
+      />
     </div>
   );
 };
