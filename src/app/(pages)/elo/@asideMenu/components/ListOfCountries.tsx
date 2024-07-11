252a24d7e4fa965dbd887aef13b6af5eb2356749
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-11 13:55:48 +0100

    created

--- src/app/(pages)/elo/@asideMenu/components/ListOfCountries.tsx
+++ src/app/(pages)/elo/@asideMenu/components/ListOfCountries.tsx
@@ -1,7 +1,4 @@
-import { getCountries } from "@/app/routes/Stats_Between_Teams/routes";
-import Image from "next/image";
-import React from "react";
-import { Config } from "../../../../../../Config";
+import FilteredCountries from "./FIlteredCountries";
 
 const ListOfCountries = async ({
   query,
@@ -15,29 +12,7 @@ const ListOfCountries = async ({
   const filteredCountries = countries?.filter((country: any) =>
     country?.country.toLowerCase().includes(query.toLowerCase())
   );
-  console.log("Filtered countries: ", filteredCountries.length);
-  return (
-    <div>
-      <ul className="space-y-2 py-2 max-h-96 overflow-y-auto">
-        {filteredCountries?.map((country: any, index: number) => (
-          <li
-            key={`${country}-${index}`}
-            className="hover:bg-slate-100 px-2 cursor-pointer flex gap-2 items-center antialiased"
-          >
-            <Image
-              src={`${
-                Config.baseURL
-              }/${country?.images_country[0]?.img_path.replace("/", "")}`}
-              alt={country?.country}
-              width={20}
-              height={20}
-            />
-            {country?.country.replaceAll("_", " ")}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <>{<FilteredCountries filteredCountries={filteredCountries} />}</>;
 };
 
 export default ListOfCountries;
