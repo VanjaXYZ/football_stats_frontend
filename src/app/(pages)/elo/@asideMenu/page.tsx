ef195f34419f7b88434700684816d1bee9abb9db
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-11 01:11:34 +0100

    pass url queris

--- src/app/(pages)/elo/@asideMenu/page.tsx
+++ src/app/(pages)/elo/@asideMenu/page.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { Suspense } from "react";
 import SearchBar from "./components/SearchBar";
 import ListOfCountries from "./components/ListOfCountries";
 
-const AsideMenu = () => {
+const AsideMenu = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) => {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
   return (
     <div className="w-96 p-2">
       <SearchBar />
-      <ListOfCountries />
+      <Suspense fallback={<p>Loading...</p>} key={query + currentPage}>
+        <ListOfCountries query={query} currentPage={currentPage} />
+      </Suspense>
     </div>
   );
 };
