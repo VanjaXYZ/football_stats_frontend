b09644397853da668254eb2d73b883d46584af7e
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-11 01:11:44 +0100

    created filtered countries fn

--- src/app/(pages)/elo/@asideMenu/components/ListOfCountries.tsx
+++ src/app/(pages)/elo/@asideMenu/components/ListOfCountries.tsx
@@ -1,13 +1,26 @@
 import { getCountries } from "@/app/routes/Stats_Between_Teams/routes";
 import React from "react";
 
-const ListOfCountries = async () => {
+const ListOfCountries = async ({
+  query,
+  currentPage,
+}: {
+  query: string;
+  currentPage: number;
+}) => {
   const countries = await getCountries();
+  const filteredCountries = countries?.filter((country: any) =>
+    country?.country.toLowerCase().includes(query.toLowerCase())
+  );
+  console.log("Filtered countries: ", filteredCountries);
   return (
     <div>
       <ul>
-        {countries?.map((country: any) => (
-          <li key={country} className="hover:bg-slate-100 px-2 cursor-pointer">
+        {filteredCountries?.map((country: any, index: number) => (
+          <li
+            key={`${country}-${index}`}
+            className="hover:bg-slate-100 px-2 cursor-pointer"
+          >
             {country?.country.replaceAll("_", " ")}
           </li>
         ))}
