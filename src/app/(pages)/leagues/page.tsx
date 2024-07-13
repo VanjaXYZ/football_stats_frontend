66281f5f5c166c13de6ef0afc4554d24194afadc
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-13 23:56:03 +0100

    add

--- src/app/(pages)/leagues/page.tsx
+++ src/app/(pages)/leagues/page.tsx
@@ -2,18 +2,31 @@
 import React from "react";
 
 import leaguesData from "../../../lib/leagues-data.json";
+import TableComponent from "../(shared)/TableComponent";
 
 const Page = () => {
   console.log(leaguesData);
+
+  const columnData = [
+    {
+      key: "country",
+      header: "Country",
+    },
+    {
+      key: "date",
+      header: "Data start at",
+      Component: (item: any) => (
+        <div>{new Date(item?.date).toLocaleDateString("en-GB")}</div>
+      ),
+    },
+    {
+      key: "league",
+      header: "League",
+    },
+  ];
   return (
-    <div className="mt-20 grid md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-6 p-6">
-      {leaguesData?.map((league: any) => (
-        <div>
-          <h3>Country: {league?.country}</h3>
-          <p>Start at: {league?.date}</p>
-          <p>League: {league?.league}</p>
-        </div>
-      ))}
+    <div className="mt-20 container py-6">
+      <TableComponent row={leaguesData} column={columnData} />
     </div>
   );
 };
