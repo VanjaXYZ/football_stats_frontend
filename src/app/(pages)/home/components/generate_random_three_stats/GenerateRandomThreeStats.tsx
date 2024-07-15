0fef58f728fabdfb427553b4dd8f1b6c8b258f4d
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-15 15:28:42 +0100

    test

--- src/app/(pages)/home/components/generate_random_three_stats/GenerateRandomThreeStats.tsx
+++ src/app/(pages)/home/components/generate_random_three_stats/GenerateRandomThreeStats.tsx
@@ -9,7 +9,7 @@ const GenerateRandomThreeStats = async () => {
       <h2 className="text-center font-bold text-4xl uppercase py-8">
         Random stats
       </h2>
-      <RandomThreeStatsTables data={randomThreeStats} />
+      {/* <RandomThreeStatsTables data={randomThreeStats} /> */}
     </div>
   );
 };
