51e66cded113d43894dca0e93d88eaf1a71326f8
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-08-30 10:52:30 +0100

    add elo

--- src/app/(pages)/home/stats_between_teams/TeamVsTeamHeader.tsx
+++ src/app/(pages)/home/stats_between_teams/TeamVsTeamHeader.tsx
@@ -3,13 +3,24 @@ import React from "react";
 const TeamVsTeamHeader = ({
   homeTeam,
   awayTeam,
+  eloHomeTeam,
+  eloAwayTeam,
 }: {
   homeTeam: string;
   awayTeam: string;
+  eloHomeTeam?: number;
+  eloAwayTeam?: number;
 }) => {
   return (
     <h2 className="text-4xl py-6 text-center font-semibold antialiased">
-      {homeTeam} {awayTeam && `vs ${awayTeam}`}
+      <span className="flex flex-col">
+        <span>{homeTeam}</span>
+        <span className="text-base">ELO: {eloHomeTeam}</span>
+      </span>{" "}
+      <span className="flex flex-col">
+        <span>{awayTeam && `vs ${awayTeam}`}</span>
+        <span className="text-base">ELO: {eloAwayTeam}</span>
+      </span>{" "}
     </h2>
   );
 };
