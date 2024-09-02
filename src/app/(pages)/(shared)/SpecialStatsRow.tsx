5f4c38fd80b8b02696bb81f6c709b89a6fa89489
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-09-02 23:19:27 +0100

    created

---
+++ src/app/(pages)/(shared)/SpecialStatsRow.tsx
@@ -0,0 +1,96 @@
+"use client";
+import { TableCell, TableRow } from "@/components/ui/table";
+import axios from "axios";
+import { useState } from "react";
+import { Config } from "../../../../Config";
+
+const SpecialStatsRow = ({ index, column, item }: any) => {
+  const [specialStats, setSpecialStats] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  let specialStatsCountry = item?.country_2_to_1?.country
+    ? item?.country_2_to_1?.country
+    : item?.country_plus_7_all?.country;
+
+  let specialStatsURL = item?.country_2_to_1?.country ? "2_1" : "plus_7";
+
+  const handleGamesPlayed = async (
+    country: string,
+    team_1: string,
+    team_2: string
+  ) => {
+    try {
+      const response = await axios.get(
+        `${Config.baseURL}/stats/dates_of_games_${specialStatsURL}/${country}/${team_1}/${team_2}`
+      );
+      if (response.status === 200) {
+        setSpecialStats(response.data);
+        console.log(response.data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const specialHeaders = [
+    {
+      key: "result",
+      header: "Result",
+      Component: (item: any) => (
+        <div className="w-full flex flex-col">
+          <span className="w-fit">Result: {item?.result}</span>
+          <span className="w-fit">Halftime: {item?.halfTime}</span>
+          <span className="w-fit">Date: {item?.date}</span>
+        </div>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <TableRow
+        key={`row-${index}`}
+        onClick={() => setIsOpen((prevValue: boolean) => !prevValue)}
+      >
+        {column?.map((data: any, cellIndex: number) => (
+          <TableCell
+            key={`cellID-${cellIndex}`}
+            className="text-center font-semibold p-1"
+            onClick={() =>
+              handleGamesPlayed(specialStatsCountry, item.team_1, item.team_2)
+            }
+          >
+            {data?.Component ? data?.Component(item) : item[data?.key]}
+          </TableCell>
+        ))}
+      </TableRow>
+
+      {isOpen && (
+        <TableRow className="w-full flex border-b-0">
+          {specialHeaders?.map((stats: any, index: number) =>
+            specialStats?.map((special: any, indexSpecial: number) => (
+              <TableCell key={indexSpecial} className="w-full">
+                {/* {stats?.Component
+                  ? stats?.Component(special)
+                  : special[stats?.key]} */}
+                <div className="w-full flex flex-col">
+                  <span className="w-fit font-semibold text-xs">
+                    Result: {special?.result}
+                  </span>
+                  <span className="w-fit font-semibold text-xs">
+                    Halftime: {special?.halfTime}
+                  </span>
+                  <span className="w-fit font-semibold text-xs">
+                    Date: {special?.date}
+                  </span>
+                </div>
+              </TableCell>
+            ))
+          )}
+        </TableRow>
+      )}
+    </>
+  );
+};
+
+export default SpecialStatsRow;
